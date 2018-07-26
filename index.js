12bbdccba3906c5df495fff6d2bb15b103413632
Kirby Campbell <jkirbycampbell@gmail.com>
2018-07-26 20:28:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,4 +11,4 @@ let newID = oldID + 1000000000;
 let ageIsValid = Number.isInteger(currentAge);
 let randomNumber = Math.random() * 20;
 let randomInteger = Math.floor(randomNumber);
-const randomUserId = Math.ceil(randomInteger) + 1000000001;
+const randomUserID = Math.ceil(randomInteger) + 1000000001;
